ede96809d1bd07ffe9dce5e776abd7565e1b5946
gzk47 <98312059+gzk47@users.noreply.github.com>
2025-11-26 10:52:06 +0800

    Update ui_TeslaExtras.hpp

--- overlay/include/ui/ui_TeslaExtras.hpp
+++ overlay/include/ui/ui_TeslaExtras.hpp
@@ -401,7 +401,7 @@ namespace ui {
 
                 renderer->drawRect(15, tsl::cfg::FramebufferHeight - 73, tsl::cfg::FramebufferWidth - 30, 1, a(tsl::style::color::ColorText));
 
-                renderer->drawString("\uE0E1  " + tr::Translate("Back") + "     \uE0E0  " + tr::Translate("OK"), false, 30, 693, 23, a(tsl::style::color::ColorText));
+    renderer->drawString(("\uE0E1  " + tr::Translate("Back") + "     \uE0E0  " + tr::Translate("OK")).c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
 
                 if (this->m_topSection != nullptr)
                     this->m_topSection->frame(renderer);
